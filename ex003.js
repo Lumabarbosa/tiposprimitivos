64c92678eb7eedaa3815584ec4c8763f5cb168c0
Lumabarbosa <140550663+Lumabarbosa@users.noreply.github.com>
2023-08-15 16:35:13 -0300

    Add files via upload

---
+++ ex003.js
@@ -0,0 +1,40 @@
+let aposta
+let valorAposta
+let numPremio
+let nome 
+let cpf
+
+console.log(aposta)
+console.log(valorAposta)
+console.log(numPremio)
+console.log(nome)
+console.log(cpf)
+
+aposta = null
+valorAposta = null
+numPremio = null
+nome = null
+cpf = null
+console.log(`O valor da var aposta é ${aposta}`)
+console.log(`O valor da var aposta é ${valorAposta}`)
+console.log(`O valor da var premio é ${numPremio}`)
+console.log(`O valor da var nome é ${nome}`)
+console.log(`O valor da var cpf é ${cpf}`)
+
+aposta = true
+valorAposta = 5
+numPremio = Symbol('9007199254741000')
+nome = 'José'
+cpf = 13092176534
+
+console.log(typeof aposta)
+console.log(typeof valorAposta)
+console.log(typeof numPremio)
+console.log(typeof nome)
+console.log(typeof cpf)
+
+console.log(aposta)
+console.log(valorAposta)
+console.log(numPremio)
+console.log(nome)
+console.log(cpf)
